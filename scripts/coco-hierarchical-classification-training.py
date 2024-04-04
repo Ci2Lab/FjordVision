2115fcedf4b61b3aa4667b77262baf7ecb1301e2
bcwein <bcwein343@gmail.com>
2024-04-04 13:05:43 +0200

    work in progress.

--- scripts/coco-hierarchical-classification-training.py
+++ scripts/coco-hierarchical-classification-training.py
@@ -18,26 +18,26 @@ from torch.optim.lr_scheduler import MultiStepLR
 
 # Populate Taxonomy
 importer = JsonImporter()
-with open('data/ontology.json', 'r') as f:
+with open('data/coco.json', 'r') as f:
     root = importer.read(f)
 
-classes_file = '/mnt/RAID/datasets/label-studio/fjord/classes.txt'
+classes_file = '/mnt/RAID/datasets/coco/classes.txt'
 
-species_names = []
+object_names = []
 with open(classes_file, 'r') as file:
-    species_names = [line.strip() for line in file]
+    object_names = [line.strip() for line in file]
 
-genus_names, class_names, binary_names = [], [], []
+subcategory_names, category_names, binary_names = [], [], []
 for node in root.descendants:
-    if node.rank == 'genus':
-        genus_names.append(node.name)
-    elif node.rank == 'class':
-        class_names.append(node.name)
+    if node.rank == 'subcategory':
+        subcategory_names.append(node.name)
+    elif node.rank == 'category':
+        category_names.append(node.name)
     elif node.rank == 'binary':
         binary_names.append(node.name)
 
 # Read Dataset
-df = pd.read_parquet('/mnt/RAID/projects/FjordVision/data/segmented-objects-dataset.parquet')
+df = pd.read_parquet('/mnt/RAID/projects/FjordVision/data/coco-segmented-objects-dataset.parquet')
 
 train_val_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
 train_df, val_df = train_test_split(train_val_df, test_size=0.5, random_state=42)
@@ -47,7 +47,7 @@ rank_counts = defaultdict(int)
 for node in root.descendants:
     rank_counts[node.rank] += 1
 
-num_classes_hierarchy = [rank_counts['binary'], rank_counts['class'], rank_counts['genus'], rank_counts['species']]
+num_classes_hierarchy = [rank_counts['binary'], rank_counts['category'], rank_counts['subcategory'], rank_counts['object']]
 num_additional_features = 3
 
 # Define num_levels here based on the length of num_classes_hierarchy
@@ -57,16 +57,16 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = HierarchicalCNN(num_classes_hierarchy, num_additional_features).to(device)
 
 # DataLoader
-train_dataset = CustomDataset(train_df, species_names, genus_names, class_names, binary_names, root)
-val_dataset = CustomDataset(val_df, species_names, genus_names, class_names, binary_names, root)
-test_dataset = CustomDataset(test_df, species_names, genus_names, class_names, binary_names, root)
+train_dataset = CustomDataset(train_df, object_names, subcategory_names, category_names, binary_names, root)
+val_dataset = CustomDataset(val_df, object_names, subcategory_names, category_names, binary_names, root)
+test_dataset = CustomDataset(test_df, object_names, subcategory_names, category_names, binary_names, root)
 
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 # Specify the initial value of alpha and whether it should be learnable
-initial_alpha = 1  # Example initial value
+initial_alpha = 0.5  # Example initial value
 alpha_learnable = False  # Set True if alpha should be learnable, False otherwise
 
 # Training Preparation
@@ -80,7 +80,7 @@ optimizer = torch.optim.Adam(list(model.parameters()) + list(criterion.parameter
 scheduler = MultiStepLR(optimizer, milestones=[20, 40, 60, 80], gamma=0.1)
 
 # Logging Setup
-with open('logs/model_alpha_1.csv', mode='w', newline='') as file:
+with open('logs-coco/model_alpha_05.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     # Adjust headers to include lambda weights; assume max number of levels is known
     headers = ['Epoch', 'Training Loss', 'Validation Loss', 'Alpha'] + [f'Lambda Weight Lvl {i+1}' for i in range(num_levels)]
@@ -132,7 +132,7 @@ with open('logs/model_alpha_1.csv', mode='w', newline='') as file:
         # Early stopping and model checkpointing
         if val_loss < best_val_loss:
             best_val_loss = val_loss
-            torch.save(model.state_dict(), 'models/weights/best_model_alpha_1.pth')
+            torch.save(model.state_dict(), 'models/weights/coco_best_model_alpha_05.pth')
             patience_counter = 0
         else:
             patience_counter += 1
@@ -143,4 +143,4 @@ with open('logs/model_alpha_1.csv', mode='w', newline='') as file:
         scheduler.step()
 
         # Save the last model
-        torch.save(model.state_dict(), 'models/weights/last_model_alpha_1.pth')
+        torch.save(model.state_dict(), 'models/weights/coco_last_model_alpha_05.pth')
