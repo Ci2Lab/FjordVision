d5a3e7b19ec77ead8837abc02d8de8e552de82ee
bcwein <bcwein343@gmail.com>
2024-04-08 08:22:32 +0200

    script for running experiments

---
+++ own_coco_train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /mnt/RAID/projects/FjordVision
+
+# Activate venv
+source fjordvision/bin/activate
+
+# Define a list of alpha values
+alpha_values=(0 0.2 0.5 0.8 1)
+
+# Loop through each alpha value
+for alpha_value in "${alpha_values[@]}"
+do
+    echo "Running hierarchical classification training with alpha = $alpha_value"
+    
+    # Run the Python script with the alpha argument
+    python3 scripts/coco-hierarchical-classification-training.py --alpha $alpha_value
+done
