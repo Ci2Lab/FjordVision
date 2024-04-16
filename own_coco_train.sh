e39405deef318ec6d123ed1ca8f0384b49452dc1
bcwein <bcwein343@gmail.com>
2024-04-16 11:00:50 +0200

    updated training script to handle more experiments.

--- own_coco_train.sh
+++ own_coco_train.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 cd /mnt/RAID/projects/FjordVision
 
-# Activate venv
+# Activate virtual environment
 source fjordvision/bin/activate
 
 # Define a list of alpha values
 alpha_values=(0 0.2 0.5 0.8 1)
 
-# Loop through each alpha value
-for alpha_value in "${alpha_values[@]}"
+# List of experiments to run
+declare -A experiments
+experiments["ablation-remove-features-coco"]="scripts/train-ablations/coco-hierarchical-classification-training.py"
+experiments["ablation-remove-features-fjord"]="scripts/train-ablations/hierarchical-classification-training.py"
+
+# Loop through each experiment
+for experiment in "${!experiments[@]}"
 do
-    echo "Running COCO hierarchical classification training with alpha = $alpha_value"
-    # Run the COCO Python script with the alpha argument
-    python3 scripts/coco-hierarchical-classification-training.py --alpha $alpha_value
+    script=${experiments[$experiment]}
+    echo "Running $experiment experiments:"
+    # Loop through each alpha value
+    for alpha_value in "${alpha_values[@]}"
+    do
+        echo "Running $experiment classification training with alpha = $alpha_value using script $script"
+        # Run the experiment script with the alpha argument
+        python3 $script --alpha $alpha_value
+    done
 done
