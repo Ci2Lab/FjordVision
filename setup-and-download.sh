38740b685728ced640df49ff07da7e505d3ddac4
bcwein <bcwein343@gmail.com>
2024-07-09 13:25:20 +0200

    new script for downloading data

--- setup-and-download.sh
+++ setup-and-download.sh
@@ -12,27 +12,27 @@ pip install -r requirements.txt
 echo "Setup complete. Environment is ready."
 
 #!/bin/bash
-cd "$(dirname "$0")"c
+cd "$(dirname "$0")"
 
 # Ensure the base dataset directory exists
 mkdir -p datasets
 
-# Download and unzip 'The Fjord Dataset' from Figshare
-if [ ! -f "datasets/The Fjord Dataset/fjord.yaml" ]; then
-    echo "Downloading 'The Fjord Dataset'..."
-    wget -O fjord_dataset.zip 'https://figshare.com/ndownloader/files/45433267'
-    unzip -o fjord_dataset.zip -d datasets
-    rm fjord_dataset.zip
-    echo "'The Fjord Dataset' is ready."
+# Download and unzip 'Esefjorden Marine Vegetation Segmentation Dataset' from Figshare
+if [ ! -f "datasets/EMVSD/EMVSD.yaml" ]; then
+    echo "Downloading 'Esefjorden Marine Vegetation Segmentation Dataset'..."
+    wget -O EMVSD.zip 'https://figshare.com/ndownloader/files/47512904'
+    unzip -o EMVSD.zip -d datasets
+    rm EMVSD.zip
+    echo "'Esefjorden Marine Vegetation Segmentation Dataset' is ready."
 else
-    echo "'The Fjord Dataset' already exists."
+    echo "'Esefjorden Marine Vegetation Segmentation Dataset' already exists."
 fi
 
 # Download and unzip 'FjordVision Experimental Data' from Figshare
-if [ ! -d "datasets/hierarchical-model-weights" ] || [ ! -d "datasets/pre-trained-models/coco" ]; then
+if [ ! -d "datasets/hierarchical-model-weights" ]; then
     echo "Downloading 'FjordVision Experimental Data'..."
     wget -O experimental_data.zip 'https://figshare.com/ndownloader/files/45875268'
-    unzip -o experimental_data.zip -d .  # Extract directly to the current directory
+    unzip -o experimental_data.zip -d datasets  # Extract directly to the datasets directory
     rm experimental_data.zip
     echo "'FjordVision Experimental Data' is ready."
 else
